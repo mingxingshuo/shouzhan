616770c2f5daf2e0dbeecab6a64f920aa00b28e1
efdssdf <efdssdf@gmail.com>
2019-04-15 14:13:30 +0800

    'aa'

--- tuitui_weichat/script/aaaa.js
+++ tuitui_weichat/script/aaaa.js
@@ -5,7 +5,7 @@ var OpenidModel = require('../model/Openid')
 var wechat_util = require('../util/get_weichat_client.js')
 
 async function a() {
-    // let code = process.argv.slice(2)[0]
+    let code = process.argv.slice(2)[0]
     // await ConfigModel.update({code: code}, {status: -2})
 
     let client = await wechat_util.getClient(code)
