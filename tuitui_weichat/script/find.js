e18edf6b311ee0f01dae57262074a60cc47df603
efdssdf <efdssdf@gmail.com>
2019-07-21 07:55:20 +0800

    'script'

--- tuitui_weichat/script/find.js
+++ tuitui_weichat/script/find.js
@@ -1,11 +1,17 @@
 var UserconfModel = require('../model/Userconf')
 var OpenidModel = require('../model/Openid')
 var wechat_util = require('../util/get_weichat_client')
+var OpenidtagModel = require('../model/OpenidTag')
+var SubOpenidtagModel = require('../model/SubOpenidTag')
 
 async function a() {
     let code = process.argv.slice(2)[0]
     let client = await wechat_util.getClient(code)
 
+    let openidtag = await OpenidtagModel.count({code:code})
+    console.log(openidtag,'--------------openidtag')
+    let subopenidtag = await SubOpenidtagModel.count({code:code})
+    console.log(subopenidtag,'--------------subopenidtag')
     let openid = await OpenidModel.count({code:code})
     console.log(openid,'--------------openid')
     let user = await UserconfModel.count({code:code})
